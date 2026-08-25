d448dfa5de2b52e6fd942ee9be27e03bec0bdc5a
Alicia Vizcarra Ramos <vizcarrasheyla2@gmail.com>
2026-08-24 20:27:24 -0500

    s2: enums del dominio

---
+++ shared/src/commonMain/kotlin/pe/edu/upeu/acopioleche/domain/model/MotivoRechazo.kt
@@ -0,0 +1,8 @@
+package pe.edu.upeu.acopioleche.domain.model
+
+enum class MotivoRechazo {
+    DENSIDAD_FUERA_DE_RANGO,
+    ACIDEZ_FUERA_DE_RANGO,
+    GRASA_FUERA_DE_RANGO,
+    ADULTERACION_DETECTADA,
+}
---
+++ shared/src/commonMain/kotlin/pe/edu/upeu/acopioleche/domain/model/TipoNotificacion.kt
@@ -0,0 +1,10 @@
+package pe.edu.upeu.acopioleche.domain.model
+
+enum class TipoNotificacion {
+    RESUMEN_ENTREGA_DIARIA,
+    RESUMEN_ENTREGA_SEMANAL,
+    ALERTA_ADULTERACION,
+    RESULTADO_DENSIDAD,
+    CITACION_REUNION,
+    AVISO_CAPACITACION,
+}
